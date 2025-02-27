95003c99b4f99abc909f506650d4714538af3d06
AnasNAK <anasc00ding@gmail.com>
2025-02-27 22:58:20 +0000

    add dependencies

---
+++ src/main/java/org/NAK/microservice_E_commerce/MicroserviceECommerceApplication.java
@@ -0,0 +1,13 @@
+package org.NAK.microservice_E_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/NAK/microservice_E_commerce/MicroserviceECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.NAK.microservice_E_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
